06aa2b5786a5e72cfd5fddb1c622a68debcdd73d
Austin Fast <austinfast.news@gmail.com>
2025-01-30 17:10:18 -0500

    Compress/repair all maps

--- 250130-FixSeason.R
+++ 250130-FixSeason.R
@@ -1,17 +1,37 @@
 library (sf)
 library (geojsonio)
 
-file2 <- "outputs/season/season_inches_snow_accumulation_latest.json"
+# file2 <- "outputs/season/season_inches_snow_accumulation_latest.json"
+# 
+# squeezed <- topojson_read(file2)
+# 
+# #remove slivers on edges
+# squeezed2 <- squeezed %>% 
+#   #st_make_valid() %>%
+#   st_simplify(dTolerance = 0.01)
+# 
+# #save fixed file
+# geojsonio::topojson_write(squeezed2, 
+#                           file = file2,
+#                           object_name = "snowfall", 
+#                           overwrite = TRUE)
 
-squeezed <- topojson_read(file2)
+timeframes <- c("24h", "48h", "72h", "season")
 
-#remove slivers on edges
-squeezed2 <- squeezed %>% 
-  #st_make_valid() %>%
-  st_simplify(dTolerance = 0.01)
-
-#save fixed file
-geojsonio::topojson_write(squeezed2, 
-                          file = file2,
-                          object_name = "snowfall", 
-                          overwrite = TRUE)
+lapply(1:length(timeframes), function (x){
+  timeframe <- timeframes[[x]]
+  file <- paste0("outputs/", timeframe, "/", timeframe, "_inches_snow_accumulation_latest.json")
+  
+  squeezed <- topojson_read(file)
+  
+  #remove slivers on edges
+  squeezed2 <- squeezed %>% 
+    #st_make_valid() %>%
+    st_simplify(dTolerance = 0.01)
+  
+  #save fixed file
+  geojsonio::topojson_write(squeezed2, 
+                            file = file,
+                            object_name = "snowfall", 
+                            overwrite = TRUE)
+})
